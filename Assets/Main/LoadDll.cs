2a032b689959497a5dddcc4c0edf83d641b74874
bianchui <bianchui@gmail.com>
2022-05-04 16:14:56 +0800

    test loadAB from net

--- Assets/Main/LoadDll.cs
+++ Assets/Main/LoadDll.cs
@@ -4,31 +4,45 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LoadDll : MonoBehaviour
 {
     void Start()
     {
+        StartCoroutine(StartCo());
+    }
+
+    private IEnumerator StartCo() {
         BetterStreamingAssets.Initialize();
-        LoadGameDll();
+        yield return LoadGameDll(); 
         RunMain();
     }
 
     private System.Reflection.Assembly gameAss;
 
-    private void LoadGameDll()
+    private const string HttpRoot = "http://192.168.31.13:8000";
+    
+    private IEnumerator LoadGameDll()
     {
-        AssetBundle dllAB = BetterStreamingAssets.LoadAssetBundle("common");
+        AssetBundle dllAB;
+        const string url = HttpRoot + "/common";
 #if !UNITY_EDITOR
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url);
+        yield return request.SendWebRequest();
+        dllAB = DownloadHandlerAssetBundle.GetContent(request);
+
         TextAsset dllBytes1 = dllAB.LoadAsset<TextAsset>("HotFix.dll.bytes");
         System.Reflection.Assembly.Load(dllBytes1.bytes);
         TextAsset dllBytes2 = dllAB.LoadAsset<TextAsset>("HotFix2.dll.bytes");
         gameAss = System.Reflection.Assembly.Load(dllBytes2.bytes);
 #else
+        dllAB = BetterStreamingAssets.LoadAssetBundle("common");
         gameAss = AppDomain.CurrentDomain.GetAssemblies().First(assembly => assembly.GetName().Name == "HotFix2");
 #endif
 
         GameObject testPrefab = GameObject.Instantiate(dllAB.LoadAsset<UnityEngine.GameObject>("HotUpdatePrefab.prefab"));
+        yield break;
     }
 
     public void RunMain()
